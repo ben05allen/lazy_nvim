11074f28a9bd0ca65b3d63a2cc0377451ce6302a
ben05allen <ben05allen@hotmail.com>
2024-12-04 18:15:12 +0900

    init

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,10 @@
+
+return {
+  -- adding undotree so I can make loads of mistakes
+  {
+    'mbbill/undotree',
+    config = function()
+      vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    end
+  }
+}
