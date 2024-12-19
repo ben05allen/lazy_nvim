306bd9a0123a7e3ad6c48a9c38be232da8944995
ben05allen <ben05allen@hotmail.com>
2024-12-19 15:27:14 +0900

    adding new keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set({'i', 'v'}, 'jj', '<Esc>', { noremap = true })
+
+-- use ; instead of : for commands
+vim.keymap.set('n', ';', ':', { noremap = true })
+
+-- faster window navigation
+vim.keymap.set('n', '<C-h>', ':wincmd h', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-j>', ':wincmd j', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-k>', ':wincmd k', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-l>', ':wincmd l', { noremap = true, silent = true })
