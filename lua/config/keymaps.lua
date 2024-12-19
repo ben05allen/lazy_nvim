1d43220752cd1ac39c0bca7358c0a75bb8ef4601
ben05allen <ben05allen@hotmail.com>
2024-12-19 16:35:19 +0900

    adding command key remap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set({'i', 'v'}, 'jj', '<Esc>', { noremap = true })
+
+-- Remap command key 
+vim.keymap.set('n', ';', ':', {remap = true})
+
 -- Faster window navigation
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>', {remap = true})
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>', {remap = true})
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>', {remap = true})
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>', {remap = true})
