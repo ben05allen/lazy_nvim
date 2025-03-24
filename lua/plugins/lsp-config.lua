bcce4231dac63f387844f8776bfb088e36516f03
ben05allen <ben05allen@hotmail.com>
2025-03-25 08:30:31 +0900

    fixing ruff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,27 +1,30 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function ()
-      require("mason").setup()
-    end
-
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function ()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls", "eslint", "ts_ls", "volar", "basedpyright", "ruff"
-        }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function ()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup{}
-      lspconfig.basedpyright.setup{}
-    end
-  }
+  "williamboman/mason.nvim",
+  config = function ()
+    require("mason").setup()
+  end
 }
+--  {
+--     "williamboman/mason-lspconfig.nvim",
+--     config = function ()
+--       require("mason-lspconfig").setup({
+--         ensure_installed = {"lua_ls", "basedpyright", "ruff"}
+--       })
+--     end
+--   },
+--   {
+--     "neovim/nvim-lspconfig",
+--     config = function ()
+--       local lspconfig = require("lspconfig")
+--       lspconfig.lua_ls.setup{}
+--       lspconfig.basedpyright.setup{}
+--       lspconfig.ruff.setup({
+--         init_options = {
+--           settings = {
+--             -- Ruff language server settings go here
+--           }
+--         }
+--       })
+--     end
+--   }
+-- }
