1d8fe94b13f4b823f9c186630891aa0ee16073d7
Ben Allen <ben05allen@hotmail.com>
2025-08-03 16:49:53 +0900

    fix for venv-select config

--- lua/plugins/venv-select.lua
+++ lua/plugins/venv-select.lua
@@ -1,19 +1,11 @@
 return {
-  -- add support for python venvs
-  {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
-      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-    },
-    lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
-    config = function()
-      require("venv-selector").setup()
-    end,
-    keys = {
-      { ",v", "<cmd>VenvSelect<cr>" },
-    },
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig", "mfussenegger/nvim-dap-python", "nvim-telescope/telescope.nvim"
+  },
+  event = 'VeryLazy',
+  keys = {
+    { "<leader>vs", "<cmd>VenvSelect<cr>" },
+    { "<leader>vc", "<cmd>VenvSlectCached<cr>" },
   },
 }
