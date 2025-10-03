418fa9c48001fe5ece41142cce1620787bf3c7ca
Ben Allen <ben05allen@hotmail.com>
2025-10-03 22:57:00 +0900

    updating venv-select to use main branch and new name for mason

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,30 +1,6 @@
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
   config = function ()
     require("mason").setup()
   end
 }
---  {
---     "williamboman/mason-lspconfig.nvim",
---     config = function ()
---       require("mason-lspconfig").setup({
---         ensure_installed = {"lua_ls", "basedpyright", "ruff"}
---       })
---     end
---   },
---   {
---     "neovim/nvim-lspconfig",
---     config = function ()
---       local lspconfig = require("lspconfig")
---       lspconfig.lua_ls.setup{}
---       lspconfig.basedpyright.setup{}
---       lspconfig.ruff.setup({
---         init_options = {
---           settings = {
---             -- Ruff language server settings go here
---           }
---         }
---       })
---     end
---   }
--- }
