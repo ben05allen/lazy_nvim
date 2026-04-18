17ba69405f74b2d67511188ca88801c471803751
Ben Allen <ben05allen@hotmail.com>
2026-04-18 22:53:11 +0900

    adding grug-far

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*", -- or {"*.Dockerfile", "*.lua", "*.py", ...} to limit it
+  group = vim.api.nvim_create_augroup("trim_trailing", { clear = true }),
+  callback = function()
+    -- Preserve cursor & search
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+  desc = "Trim trailing whitespace on save",
+})
