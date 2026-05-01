2cf1039e40bc54621b0cc88b65e06c24383da2b4
Ben Allen <ben05allen@hotmail.com>
2026-05-01 16:59:15 +0900

    adding c formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,26 @@
 return {
   {
-    'stevearc/conform.nvim',
-    opts = {},
-  }
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true, -- fallback to LSP (clangd) if no formatter
+      },
+      formatters_by_ft = {
+        c = { "clang_format" },
+        cpp = { "clang_format" },
+        objc = { "clang_format" },
+        cuda = { "clang_format" },
+        -- proto = { "clang_format" },  -- if you want
+      },
+      -- Optional: extra arguments for clang-format
+      formatters = {
+        clang_format = {
+          prepend_args = { "--style=file", "--fallback-style=LLVM" },
+          -- "--style=file" makes it automatically find .clang-format in parent dirs
+        },
+      },
+    },
+  },
 }
